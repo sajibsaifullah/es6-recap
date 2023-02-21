c38d4708ef3040e8ad8a0bc1ef2ce30ae2251ca5
sajibsaifullah <dusajibphy@gmail.com>
2023-02-21 21:09:48 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,37 @@
+/* 
+Must Know
+1. var, let, const
+
+2. template string
+    2.1: create multi-line string(``)
+    2.2: add dynamic variable (expression): ${}
+3. Arrow function
+    3.1: single parameter arrow function
+    3.2: two parameter arrow function
+    3.3: arrow function with function body. Amd don't forget you have to return explicitly.
+4. map, filter, find
+5. optional chaining
+
+*/
+
+// GOOD TO HAVE
+/* 
+    1. spread operator [...numbers, 12, 45]
+    2. destructuring 
+        2-1: object destructuring. Hoe to get property name as variable
+            const {x} = {x:3, y: 5}
+        2.2: array destructuring
+            const[a, b] = [3, 8, 12, 56]
+    3. default parameter
+    4. forEach
+    5. dot notation vs bracket notation
+
+*/
+
+// SHOULD KNOW THE NAME
+/* 
+1. reduce
+2. inheritance
+3. prototypical chain
+4. class, object
+*/
